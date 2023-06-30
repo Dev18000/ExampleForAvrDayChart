dab52c33afe7fbd57b604a11788fffcb84cffd8c
farida.seyidzade <farida.seyidzade@laposte.net>
2023-06-30 12:34:26 +0200

    Ajoutez des fichiers projet.

---
+++ ExampleForAvrDayChart/AppSettings/ApplicationServicesSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace ExampleForAvrDayChart.AppSettings
+{
+    public static class ApplicationServicesSettings
+    {
+        public const string ApiSettings = "ApiSettings";
+        public static ApiSettingsOptions ApiSettingsOptions { get; set; } = new ApiSettingsOptions();
+    }
+
+    public class ApiSettingsOptions
+    {
+    }
+}
---
+++ ExampleForAvrDayChart/Program.cs
@@ -0,0 +1,15 @@
+using ExampleForAvrDayChart;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Syncfusion.Blazor;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(builder.Configuration["SyncfKey"]);
+builder.Services.AddSyncfusionBlazor();
+
+await builder.Build().RunAsync();
